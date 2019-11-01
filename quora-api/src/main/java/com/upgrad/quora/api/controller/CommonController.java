01be8f78853e2a60ff76945ed01f07d19235d643
aakash012 <aakash.kumar9128@gmail.com>
2019-11-01 18:44:28 +0530

    AdminController endpoint initialization

--- quora-api/src/main/java/com/upgrad/quora/api/controller/CommonController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/CommonController.java
@@ -1,16 +1,17 @@
 package com.upgrad.quora.api.controller;
 
 import com.upgrad.quora.api.model.UserDetailsResponse;
+import com.upgrad.quora.service.business.SignupBusinessService;
 import com.upgrad.quora.service.business.UserAdminBusinessService;
 import com.upgrad.quora.service.entity.UserEntity;
-import com.upgrad.quora.service.exception.AuthorizationFailedException;
-import com.upgrad.quora.service.exception.UnauthorizedException;
-import com.upgrad.quora.service.exception.UserNotFoundException;
+import com.upgrad.quora.service.exception.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/")
@@ -20,7 +21,7 @@ public class CommonController {
     private UserAdminBusinessService userAdminBusinessService;
 
     @RequestMapping(method = RequestMethod.GET, path = "/userprofile/{userId}",  produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<UserDetailsResponse> UserProfile(@PathVariable("userId") final String userUuid,@RequestHeader("access-token") final String accessToken) throws UnauthorizedException, AuthorizationFailedException, UserNotFoundException {
+    public ResponseEntity<UserDetailsResponse> UserProfile(@PathVariable("userId") final String userUuid,@RequestHeader("access-token") final String accessToken) throws AuthorizationFailedException, UserNotFoundException {
 
         final UserEntity userEntity = userAdminBusinessService.getUser(userUuid,accessToken);
         UserDetailsResponse userDetailsResponse = new UserDetailsResponse().firstName(userEntity.getFirstName())
