ef6d1b8378358b81c83f89dcf3a09a9d88d69a51
aakash012 <aakash.kumar9128@gmail.com>
2019-11-02 19:53:46 +0530

    Admin endpoint code restructured

--- quora-service/src/main/java/com/upgrad/quora/service/business/UserAdminBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/UserAdminBusinessService.java
@@ -3,60 +3,81 @@ package com.upgrad.quora.service.business;
 import com.upgrad.quora.service.dao.UserDao;
 import com.upgrad.quora.service.entity.UserAuthTokenEntity;
 import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthenticationFailedException;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.SignOutRestrictedException;
 import com.upgrad.quora.service.exception.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
 @Service
 public class UserAdminBusinessService {
-
     @Autowired
     private UserDao userDao;
-
     @Autowired
-    private UserAuthTokenValidifierService userAuthTokenValidifierService ;
+    private PasswordCryptographyProvider CryptographyProvider;
 
-    /**
-     * @param  userUuid the first {@code AnswerEntity} id to delete the user
-     * @param  authorizationToken the second {@code String} to check if the access is available.
-     * @return id of the deleted user
-     */
-    @Transactional(propagation = Propagation.REQUIRED)
-    public String deleteUser(final String userUuid, final String authorizationToken) throws
-            AuthorizationFailedException, UserNotFoundException
-    {
+    public UserEntity DeleteUser(final String userid, final String authorizationToken) throws AuthorizationFailedException, UserNotFoundException {
         UserAuthTokenEntity userAuthTokenEntity = userDao.getUserAuthToken(authorizationToken);
+        if(userAuthTokenEntity != null){
+            ZonedDateTime logout = userAuthTokenEntity.getLogoutAt();
+            if (logout != null) {
+                throw new AuthorizationFailedException("ATHR-002", "User is signed out");
+            }
 
-        //Check if user has signed-in
-        if(userAuthTokenEntity == null)
-        {
-            throw new AuthorizationFailedException("ATHR-001","User has not signed in");
+            UserEntity userEntity = userDao.getUserByUUID(userid);
+            if (userEntity == null){
+                throw new UserNotFoundException("USR-001", "User with entered uuid to be deleted does not exist");
+            }
+            else
+            {
+                String RoleName = userEntity.getRole();
+                if (!RoleName.equals("admin")){
+                    throw new AuthorizationFailedException("ATHR-003", "Unauthorized Access,Entered user is not an admin");
+                }
+
+            }
+
+            return userDao.DeleteUser(userEntity);
         }
 
-        //Check if user has signed-out
-        if(userAuthTokenValidifierService.userSignOutStatus(authorizationToken))
-        {
-            throw new AuthorizationFailedException("ATHR-002","User is signed out");
+        throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public UserAuthTokenEntity authenticate(final String username, final String password) throws AuthenticationFailedException {
+        UserEntity userEntity = userDao.getUserName(username);
+        if (userEntity == null) {
+            throw new AuthenticationFailedException("ATH-001", "This username does not exist");
         }
 
-        //Check if the user has admin privilege
-        String role = userAuthTokenEntity.getUser().getRole();
-        if(role.equals("nonadmin"))
-        {
-            throw new AuthorizationFailedException("ATHR-003","Unauthorized Access, Entered user is not an admin");
-        }
-        UserEntity userEntityToDelete = userDao.getUserByUuid(userUuid);
+        //final String encryptedPassword = CryptographyProvider.encrypt(password, userEntity.getSalt());
+        if (password.equals(userEntity.getPassword())) {
+            JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(password);
+            UserAuthTokenEntity userAuthTokenEntity = new UserAuthTokenEntity();
+            userAuthTokenEntity.setUser(userEntity);
+            final ZonedDateTime now = ZonedDateTime.now();
+            final ZonedDateTime expiresAt = now.plusHours(8);
 
-        //Check if user to be deleted is present in repository
-        if(userEntityToDelete == null)
-        {
-            throw new UserNotFoundException("USR-001", "User with entered uuid to be deleted does not exist");
-        }else{
-            userDao.deleteUser(userUuid);
-            return userUuid;
+            userAuthTokenEntity.setAccessToken(jwtTokenProvider.generateToken(userEntity.getUuid(), now, expiresAt));
+
+            userAuthTokenEntity.setLoginAt(now);
+            userAuthTokenEntity.setExpiresAt(expiresAt);
+            userAuthTokenEntity.setUuid(UUID.randomUUID().toString());
+            userDao.createAuthToken(userAuthTokenEntity);
+
+            userDao.updateUser(userEntity);
+             return userAuthTokenEntity;
+        } else {
+            throw new AuthenticationFailedException("ATH-002", "Password failed");
         }
     }
+
+
+
 }
